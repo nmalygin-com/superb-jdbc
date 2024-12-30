890a349182bce2367e839bfbdfdba24a097110bc
Nikolai Malygin <code@nmalygin.com>
2024-12-31 00:24:26 +0300

    Define abstractions in README.md

--- src/test/java/com/nmalygin/superb/jdbc/real/ReadmeTest.java
+++ src/test/java/com/nmalygin/superb/jdbc/real/ReadmeTest.java
@@ -24,7 +24,12 @@
 
 package com.nmalygin.superb.jdbc.real;
 
-import com.nmalygin.superb.jdbc.api.Rdbms;
+import com.nmalygin.superb.jdbc.api.*;
+import com.nmalygin.superb.jdbc.api.arguments.IntArgument;
+import com.nmalygin.superb.jdbc.api.arguments.ObjectArgument;
+import com.nmalygin.superb.jdbc.api.arguments.StringArgument;
+import com.nmalygin.superb.jdbc.api.handlers.ColumnToListRsh;
+import com.nmalygin.superb.jdbc.api.handlers.columns.StringColumn;
 import com.nmalygin.superb.jdbc.real.testdb.BooksTable;
 import com.nmalygin.superb.jdbc.real.testdb.DataSourceBooksTable;
 import com.nmalygin.superb.jdbc.real.testdb.H2DataSource;
@@ -33,6 +38,8 @@ import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -48,5 +55,144 @@ public class ReadmeTest {
         rdbms.change("INSERT INTO books(title) VALUES ('Clean Code')").apply();
 
         assertEquals(1, booksTable.books().size());
+    }
+
+    @Test
+    void simpleSelect() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+        booksTable.insert(UUID.randomUUID(), "Clean Code");
+
+        Queries queries = new RealRdbms(dataSource);
+        List<String> titles = queries
+                .query("SELECT title FROM books")
+                .executeWith(new ColumnToListRsh<>(new StringColumn("title")));
+
+        assertEquals(1, titles.size());
+        assertEquals("Clean Code", titles.get(0));
+    }
+
+    @Test
+    void selectWithParams() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+        booksTable.insert(UUID.randomUUID(), "Clean Code");
+
+        Queries queries = new RealRdbms(dataSource);
+        List<String> titles = queries
+                .query(
+                        "SELECT title FROM books WHERE title LIKE ?",
+                        new StringArgument("Clean%")
+                )
+                .executeWith(new ColumnToListRsh<>(new StringColumn("title")));
+
+        assertEquals(1, titles.size());
+        assertEquals("Clean Code", titles.get(0));
+    }
+
+    @Test
+    void buildingSelect() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+        booksTable.insert(UUID.randomUUID(), "Clean Code");
+
+        Queries queries = new RealRdbms(dataSource);
+
+        Query titlesQuery = queries.query("SELECT title FROM books");
+        titlesQuery.append(" WHERE title LIKE ?", new StringArgument("Clean%"));
+        titlesQuery.append(" LIMIT ?", new IntArgument(10));
+        List<String> titles = titlesQuery.executeWith(new ColumnToListRsh<>(new StringColumn("title")));
+
+        assertEquals(1, titles.size());
+        assertEquals("Clean Code", titles.get(0));
+    }
+
+    @Test
+    void changeSimpleExample() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+
+        Changes changes = new RealRdbms(dataSource);
+
+        changes
+                .change("INSERT INTO books(title) VALUES ('Clean Code')")
+                .apply();
+
+        assertEquals(1, booksTable.books().size());
+    }
+
+    @Test
+    void changeWithParams() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+
+        Changes changes = new RealRdbms(dataSource);
+
+        changes
+                .change("INSERT INTO books(title) VALUES (?)", new StringArgument("Clean Code"))
+                .apply();
+
+        assertEquals(1, booksTable.books().size());
+    }
+
+    @Test
+    void batchSimpleExample() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+
+        Batches batches = new RealRdbms(dataSource);
+
+        try (Batch batch = batches.batch("INSERT INTO books(title) VALUES (?)")) {
+            batch.put(new StringArgument("Clean Code"));
+            batch.put(new StringArgument("Code Complete"));
+            batch.put(new StringArgument("Effective Java"));
+            batch.apply();
+        }
+
+        assertEquals(3, booksTable.books().size());
+    }
+
+    @Test
+    void transactionSimpleExample() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+
+        Transactions transactions = new RealRdbms(dataSource);
+
+        try (Transaction transaction = transactions.transaction()) {
+            transaction.change("INSERT INTO books(title) VALUES ('Clean Code')").apply();
+            transaction.change("INSERT INTO books(title) VALUES ('Code Complete')").apply();
+            transaction.change("INSERT INTO books(title) VALUES ('Effective Java')").apply();
+            transaction.commit();
+        }
+
+        assertEquals(3, booksTable.books().size());
+    }
+
+    @Test
+    void transactionUsingSavepoints() throws SQLException {
+        final DataSource dataSource = new H2DataSource();
+        new LibraryDB(dataSource).init();
+        final BooksTable booksTable = new DataSourceBooksTable(dataSource);
+
+        Transactions transactions = new RealRdbms(dataSource);
+
+        try (Transaction transaction = transactions.transaction()) {
+            transaction.change("INSERT INTO books(title) VALUES ('Clean Code')").apply();
+            transaction.setSavepoint("MySavepoint");
+            transaction.change("INSERT INTO books(title) VALUES ('Code Complete')").apply();
+            transaction.rollbackTo("MySavepoint");
+            transaction.change("INSERT INTO books(title) VALUES ('Effective Java')").apply();
+            transaction.commit();
+        }
+
+        assertEquals(3, booksTable.books().size());
     }
 }
