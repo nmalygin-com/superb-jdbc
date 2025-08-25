741cfae904b80e94f106496d6b362dcf86910551
Nikolai Malygin <code@nmalygin.com>
2025-08-26 01:06:02 +0300

    #9: implemented time arguments

---
+++ src/main/java/com/nmalygin/superb/jdbc/api/arguments/DateArgument.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Nikolai Malygin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nmalygin.superb.jdbc.api.arguments;
+
+import com.nmalygin.superb.jdbc.api.Argument;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+/**
+ * A date argument
+ *
+ * @author Nikolai Malygin
+ */
+public final class DateArgument implements Argument {
+
+    private final Date value;
+
+    /**
+     * @param value LocalDate value
+     */
+    public DateArgument(final LocalDate value) {
+        if (value == null) {
+            this.value = null;
+        } else {
+            this.value = Date.valueOf(value);
+        }
+    }
+
+    @Override
+    public void pass(final PreparedStatement preparedStatement, final int position) throws SQLException {
+        preparedStatement.setDate(position, value);
+    }
+}
